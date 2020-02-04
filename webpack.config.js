37845f1af2df7fd4b9b22801da71c71782f6c446
tarasSapryniuk <44494173+tarasSapryniuk@users.noreply.github.com>
2020-02-04 19:03:19 +0200

    Webpack project deployment

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: "./src/js/index.js",
+    frontend: "./src/styles/style.sass"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      { test: /\.handlebars$/, loader: "handlebars-loader" },
+      {
+        test: /\.(sass|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {}
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browsers: ["last 2 versions"]
+              },
+              plugins: () => [autoprefixer]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: "imports-loader?jQuery=jquery"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        handlebarsLoader: {}
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]-styles.css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      title: "GSM Server",
+      template: "./src/index.handlebars"
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Tether: "tether"
+    })
+  ]
+};
